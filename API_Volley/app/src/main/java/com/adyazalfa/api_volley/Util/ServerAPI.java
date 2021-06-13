c6ffb7395c23f99271afbc232281a07e95f5996f
AdyaZalfaZahirah <66558026+AdyaZalfaZahirah@users.noreply.github.com>
2021-06-13 18:31:56 +0700

    E31190957_AdyaZalfaZahirah_B_AndroidAPI

---
+++ API_Volley/app/src/main/java/com/adyazalfa/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.adyazalfa.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.206/TugasWeb/B_E31190957_AdyaZalfaZahirah_Web/Rest_API/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.206/TugasWeb/B_E31190957_AdyaZalfaZahirah_Web/Rest_API/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.206/TugasWeb/B_E31190957_AdyaZalfaZahirah_Web/Rest_API/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.206/TugasWeb/B_E31190957_AdyaZalfaZahirah_Web/Rest_API/Mahasiswa/ApiUpdate";
+}
